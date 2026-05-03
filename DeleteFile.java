8ae46031d37833a41a8d31df60caa16cc8dc3a82
Mahdieh <mahdiehhagikarim@gmail.com>
2026-05-03 14:06:33 +0330

    Create DeleteFile.java

---
+++ DeleteFile.java
@@ -0,0 +1,14 @@
+package File;
+
+import java.io.File;
+
+public class DeleteFile {
+    public static void main(String[] args) {
+        File myFile = new File("test.txt");
+        if(myFile.delete()) {
+            System.out.println("Deleted the file:" + myFile.getName());
+        } else {
+            System.out.println("Failed to delete the file.");
+        }
+    }
+}
